c40f76144d8e33eb0f1e44d9fbf93e02c8ceb371
Sergey <sergeychikin1981@gmail.com>
2023-03-27 20:48:14 +0300

    first commit

---
+++ src/main/java/pro/sky/skyprospringemployee/SkyproSpringEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringEmployeeApplication.class, args);
+	}
+
+}
